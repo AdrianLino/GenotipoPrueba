17c73d22a20ca4cbce24e58c95d623cc5260c6ec
adrianchino15 <adrian_w15@outlook.com>
2024-04-01 23:28:11 -0600

    apartado de inicio de sesion y control de usuarios

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,24 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+$routes->group('', ['namespace' => 'Myth\Auth\Controllers'], function($routes) {
+    // Rutas de Login
+    $routes->get('login', 'AuthController::login', ['as' => 'login']);
+    $routes->post('login', 'AuthController::attemptLogin');
+    $routes->get('logout', 'AuthController::logout');
+
+    // Rutas de Registro
+    $routes->get('register', 'AuthController::register', ['as' => 'register']);
+    $routes->post('register', 'AuthController::attemptRegister');
+
+    // Rutas para la gestión de la contraseña
+    $routes->get('forgot', 'AuthController::forgotPassword', ['as' => 'forgot']);
+    $routes->post('forgot', 'AuthController::attemptForgot');
+    $routes->get('reset-password', 'AuthController::resetPassword', ['as' => 'reset-password']);
+    $routes->post('reset-password', 'AuthController::attemptReset');
+});
+
+// Protege las rutas que requieran autenticación.
+$routes->get('dashboard', 'DashboardController::index', ['filter' => 'login']);
+
